9112e0443fee1cb61e8838364f81fbf65cad304e
Ammar Yousuf Abrahani <60477445+ammarabrahani@users.noreply.github.com>
2026-06-25 23:17:38 +0100

    Refactor validBST.js by cleaning up comments
    Removed comments and improved code formatting.

--- validBST.js
+++ validBST.js
@@ -1,25 +1,9 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- * constructor(val = 0, left = null, right = null) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
-
 class Solution {
-    /**
-     * @param {TreeNode} root
-     * @return {boolean}
-     */
     isValidBST(root) {
         const dfs = (node, minVal, maxVal) => {
             if (node === null) {
                 return true;
             }
-
             // Check karo kya current node boundaries tod raha hai?
             if (minVal !== null && node.val <= minVal) {
                 return false;
@@ -27,10 +11,8 @@ class Solution {
             if (maxVal !== null && node.val >= maxVal) {
                 return false; 
             }
-
             return dfs(node.left, minVal, node.val) && dfs(node.right, node.val, maxVal);
         };
-
         return dfs(root, null, null);
     }
 }
